c8f64d7e424be74d8af19af66789d8697bc33207
Iliana <iliana.pinto04@gmail.com>
2019-09-15 16:14:34 -0600

    algunos cambios

--- hadoop-word-count/WordCount/WordCount.java
+++ hadoop-word-count/WordCount/WordCount.java
@@ -75,26 +75,26 @@ public class WordCount extends Configured implements Tool {
 				throws IOException {
 			String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
 			line = line.toLowerCase();
-			String[] wordPatterns = new String[]{"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along",
+			String[] wordPatterns = new String[]{"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "along",
                 "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything",
                 "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand",
-                "behind", "being", "below", "beside", "besides", "between", "beyond", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co",
-                "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
-                "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill",
-                "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go",
+                "behind", "being", "below", "beside", "besides", "between", "beyond", "both", "but", "by", "can", "cannot", "cant", "co",
+                "con", "could", "couldnt", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "either", "else",
+                "elsewhere", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fify",
+                "first", "former", "formerly", "found", "from", "front", "further", "get", "give", "go",
                 "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself",
-                "his", "how", "however", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
+                "his", "how", "however", "ie", "if", "in", "inc", "indeed", "into", "is", "it", "its", "itself", "keep", "last", "latter",
                 "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
-                "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
-                "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+                "move", "much", "must", "my", "myself", "namely", "neither", "never", "nevertheless", "next", "nobody", "none", "noone",
+                "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "only", "onto", "or", "other", "others", "otherwise", "our",
                 "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming",
-                "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something",
-                "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence",
-                "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three",
-                "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up",
+                "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "so", "some", "somehow", "someone", "something",
+                "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "than", "that", "the", "their", "them", "themselves", "then", "thence",
+                "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though",
+                "through", "throughout", "thru", "thus", "to", "together", "too", "toward", "towards", "un", "under", "until", "up",
                 "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby",
                 "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within",
-                "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
+                "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves","for","new"};
 
 			for (int i = 33; i < 256; i++) {
 				if ((i > 64 && i < 91) || (i > 96 && i < 123)) {
@@ -301,11 +301,34 @@ public class WordCount extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(new Configuration(), new WordCount(), args);
-		System.out.println("======================================================");		
-		int count = 0;
+		System.out.println("======================================================");
+		System.out.println("******************************************************");
+		System.out.println("Original");
+		for (int i = 0; i < mostRepeatedWords.length; i++){
+			System.out.println(mostRepeatedWords[i]);
+		}
+		System.out.println("******************************************************");
+		
+		for(int i = 0; i < mostRepeatedWords.length; i++){
+			for(int j = i+1; j < mostRepeatedWords.length; j++){
+				if(mostRepeatedWords[i].equals(mostRepeatedWords[j])){
+					mostRepeatedWords[j] = " ";
+				}
+			}
+		}
+		int countI = 1;
+		for(int i = 1; i < mostRepeatedWords.length; i++){
+			if(!mostRepeatedWords[i].equals(" ")){
+				System.out.println("Top " + countI + ": " + mostRepeatedWords[i] + ", " + mostRepeatedValues[i]);
+				countI++;
+			}
+		}
+		System.out.println("******************************************************");
+		/*int count = 0;
 		String word = "";
 		String comb = "";
 		for (int i = 0; i < words.size(); i++) {
+			System.out.println("i = "+i);
 			word = words.get(i);
 			count++;
 			for (int k = 0; k < words.size(); k++) {
@@ -318,12 +341,12 @@ public class WordCount extends Configured implements Tool {
 			count = 0;
 		}
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 10; i++) {
 			top20[i] = TOP();
 		}
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Top " + (i+1) + ": " + top20[i]);
-		}
+		}*/
 		System.out.println("======================================================");
 		System.exit(res);
 	}
@@ -361,8 +384,8 @@ public class WordCount extends Configured implements Tool {
 		value_temp = "0,0";
 		return send;
 	}
-	static String[] mostRepeatedWords = { "", "", "", "", "" };
-	static int[] mostRepeatedValues = { 0, 0, 0, 0, 0 };
+	static String[] mostRepeatedWords = { "", "", "", "", "","","","","","","","","","","","","","","","" };
+	static int[] mostRepeatedValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0 };
 	static String[] top20 = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
 	static int[] itop10 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	static int checking = 0;
